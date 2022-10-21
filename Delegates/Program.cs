70721e1a6d74b4545d82bd1c5e8ba90e379d232a
bulskov <bulskov@ruc.dk>
2022-10-21 10:20:27 +0200

    delegates

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,13 +1,33 @@
 ﻿class Program
 {
     delegate int Transformer(int x);
+    delegate double MathWxp(double x);
+
 
     private static int[] numbers = new[] { 1, 2, 3, 4, 5 };
 
+    static double mypower(double i)
+    {
+        return Math.Pow(i, 2);
+    }
+
     public static void Main()
     {
+        Transformer fn = Cube;
 
+        //Console.WriteLine(fn(3));
 
+        Compute(numbers, mypower);
+        
+
+    }
+
+    static void Compute(int[] array, MathWxp fn)
+    {
+        foreach (var i in array)
+        {
+            Console.WriteLine(fn(i));
+        }
     }
 
 
--- Intro/Program.cs
+++ Intro/Program.cs
@@ -12,3 +12,26 @@ var people = new List<Person>
 var numbers = new int[] { 1, 3, 7, 2, 3, 4, 2, 5, 6 };
 
 
+//foreach (var i in numbers.Distinct().OrderBy(x => x))
+//{
+//    Console.WriteLine(i);
+//}
+
+
+//var result = new List<Person>();
+//foreach (var person in people)
+//{
+//    if (person.Age < 23)
+//    {
+//        result.Add(person);
+//    }
+//}
+
+
+//var res = people.Where(x => x.Age > 23);
+
+foreach (var person in people.OrderByDescending(person => person.Age).ThenBy(person => person.Name))
+{
+    Console.WriteLine(person);
+}
+
